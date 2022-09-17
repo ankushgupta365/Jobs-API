2dd54628fd2f9f920efc194e4e0db6ab51c85040
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-17 16:06:55 +0530

    jwt creation when user sign in implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,15 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const login = async (req,res)=>{
     res.send("user logged in")
 }
 const register = async(req,res)=>{
     const user =await User.create({...req.body})
-   res.status(StatusCodes.CREATED).json({user})
+    const token = jwt.sign({userID: user._id, name: user.name}, 'jwtsecretkeystring', {
+        expiresIn: '30d'
+    })
+   res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
 }
 
 module.exports = {
