d932e347efdcfedbeabd26a5cad2d1b5b250e9f5
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-20 15:56:33 +0530

    security packages added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//extra security pacakages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 const express = require('express');
 const app = express();
 
@@ -11,7 +17,17 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 //routers
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 const AuthenticationMiddleware = require('./middleware/authentication')
 // extra packages
 
